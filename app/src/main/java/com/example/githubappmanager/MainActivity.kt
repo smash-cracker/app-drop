e52b8593815d57d0f4c515eec78bf551bded5667
smash-cracker <dennythomas069@gmail.com>
2025-08-31 23:58:35 +0530

    splash screen

--- app/src/main/java/com/example/githubappmanager/MainActivity.kt
+++ app/src/main/java/com/example/githubappmanager/MainActivity.kt
@@ -11,12 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.githubappmanager.ui.theme.GithubAppManagerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        installSplashScreen()
         setContent {
             GithubAppManagerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
