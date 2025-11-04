20fd622ecbae067441a32633cb5a20a5f758b834
karthik <gopavarapukarthik08@gmail.com>
2025-11-05 01:21:58 +0530

    feat : connected with firebase

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,8 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "2.1.0" apply false
+
+    // Add the dependency for the Google services Gradle plugin
+    id("com.google.gms.google-services") version "4.4.4" apply false
+
 }
